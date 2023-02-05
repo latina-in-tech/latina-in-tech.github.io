04c07233c0cf9bc0c3419707988630f1601093d2
coluzziandrea <coluzziandrea.dev@gmail.com>
2023-02-05 11:44:26 +0000

    Deploying to gh-pages from @ latina-in-tech/latina-in-tech.github.io@5e638587523a021dd04f47ef40ec5499bee0ff3b 🚀

--- _next/static/PkcR7KzyB8keSANDJuaPV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/77e878d72d98d937.css","static/chunks/pages/index-eccc20e5eb96d438.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/zuQLzNZlP0bISLDg2wkPf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/77e878d72d98d937.css","static/chunks/pages/index-71e39fbe8bb31f66.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
