c51155ae1e5bb88bce295250eb952bbaac69bbc8
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2023-02-05 13:51:20 +0100

    test md to html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
+import Thumbnail from '@/components/Thumbnail';
+import { IPost } from '@/model/post';
+import { getAllPosts } from '@/utils/mdxUtils';
+import { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] });
+// props type
+type Props = {
+  posts: [IPost];
+};
 
-export default function Home() {
+const Home: NextPage<Props> = ({ posts }: Props) => {
   return (
     <>
       <Head>
@@ -23,110 +28,47 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
         <link rel='manifest' href='/manifest.json' />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              By{' '}
-              <Image
-                src='/vercel.svg'
-                alt='Vercel Logo'
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main>
+        <div>
+          <h1 className='text-4xl font-bold mb-4'>Technical articles</h1>
+
+          <div className='space-y-12'>
+            {posts.map((post) => (
+              <div key={post.slug}>
+                <div className='mb-4'>
+                  <Thumbnail
+                    slug={post.slug}
+                    title={post.title}
+                    src={post.thumbnail}
+                  />
+                </div>
+
+                <h2 className='text-2xl font-bold mb-4'>
+                  <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+                </h2>
+
+                <p>{post.description}</p>
+              </div>
+            ))}
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src='/next.svg'
-            alt='Next.js Logo'
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src='/thirteen.svg'
-              alt='13'
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
   );
-}
+};
+
+export default Home;
+
+// get posts from serverside at build time
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = getAllPosts([
+    'title',
+    'slug',
+    'date',
+    'description',
+    'thumbnail',
+  ]);
+
+  // retunr the posts props
+  return { props: { posts } };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [require('@tailwindcss/typography')],
+  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  variants: {},
+  theme: {
+    extend: {},
+  },
+  darkMode: false,
+};
