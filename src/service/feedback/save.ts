09ba82d888333c960f6dd785f31910187f87dc2c
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2023-12-01 19:01:59 +0100

    fix: adds singInAnonym & adds env vars (#44)

--- src/service/feedback/save.ts
+++ src/service/feedback/save.ts
@@ -1,16 +1,14 @@
 import { IFeedback } from '@/model/feedback';
 
-import { collection, addDoc } from 'firebase/firestore';
-import { firestoreDb } from '../firebaseConfig';
+import { getAuth, signInAnonymously } from '@firebase/auth';
+import { collection, addDoc } from '@firebase/firestore';
+import { firebaseApp, firestoreDb } from '../firebaseConfig';
 
 export const saveFeedback = async (feedback: IFeedback) => {
-  try {
-    const docRef = await addDoc(collection(firestoreDb, 'feedbacks'), {
-      ...feedback
-    });
-    console.log('Document written with ID: ', docRef.id);
-  } catch (e) {
-    console.error('Error adding document: ', e);
-    throw e;
-  }
+  await signInAnonymously(getAuth(firebaseApp)).then(() =>
+    addDoc(collection(firestoreDb, 'feedbacks'), {
+      ...feedback,
+      created_at: new Date().toISOString()
+    })
+  );
 };
--- src/service/firebaseConfig.ts
+++ src/service/firebaseConfig.ts
@@ -1,22 +1,26 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getApps, initializeApp } from '@firebase/app';
+import { getFirestore } from '@firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: '$API_KEY',
-  authDomain: '$PROJECT_ID.firebaseapp.com',
-  projectId: '$PROJECT_ID',
-  storageBucket: '$PROJECT_ID.appspot.com',
-  messagingSenderId: '$MESSAGING_SENDER_ID',
-  appId: '$APP_ID',
-  measurementId: '$MEASUREMENT_ID'
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: `${process.env.NEXT_PUBLIC_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: `${process.env.NEXT_PUBLIC_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const alreadyCreatedAps = getApps();
+const firebaseApp =
+  alreadyCreatedAps.length === 0
+    ? initializeApp(firebaseConfig, 'latina-in-tech')
+    : alreadyCreatedAps[0];
 const firestoreDb = getFirestore(firebaseApp);
 export { firebaseApp, firestoreDb };
