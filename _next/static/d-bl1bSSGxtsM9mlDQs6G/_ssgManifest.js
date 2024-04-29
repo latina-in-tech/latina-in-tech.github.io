36b19fc90d93900980e32d6de0ee4dbe08ca27f4
Undermaken <mbmaken@gmail.com>
2024-04-29 16:39:41 +0000

    Deploying to gh-pages from @ latina-in-tech/latina-in-tech.github.io@34cedc135a90f8c2bfccf60f2e6d889331d5d1a0 🚀

---
+++ _next/static/d-bl1bSSGxtsM9mlDQs6G/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fcommunity","\u002F[lang]\u002Fevents","\u002F[lang]\u002Fevents\u002F[slug]","\u002F[lang]\u002Ffeedback\u002Fnew"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
