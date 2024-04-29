4a63cf62e05c71733b8090b379e66a409c064c3d
riccardi1997 <riccardi.salvatore.97@gmail.com>
2024-04-29 17:49:40 +0200

    npm run prettify

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -80,9 +80,16 @@ export default function Custom404() {
             strokeLinejoin='round'
             className='icon'
           >
-            <circle cx="12" cy="12" r="10" stroke="#000" strokeWidth="3" />
+            <circle cx='12' cy='12' r='10' stroke='#000' strokeWidth='3' />
             <circle cx='12' cy='12' r='10'></circle>
-            <line x1='12' y1='8' x2='12' y2='16' stroke="#000" strokeWidth="3"></line>
+            <line
+              x1='12'
+              y1='8'
+              x2='12'
+              y2='16'
+              stroke='#000'
+              strokeWidth='3'
+            ></line>
             <line x1='12' y1='8' x2='12' y2='12'></line>
             <line x1='12' y1='16' x2='12' y2='16'></line>
           </svg>
